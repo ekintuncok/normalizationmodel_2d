3c241f53f999d0d0234ecae295f2533d0a1f1e9c
Ekin Tünçok <60780594+ekintuncok@users.noreply.github.com>
2024-05-23 18:23:26 -0400

    Delete matlab_scripts directory

--- matlab_scripts/functions/createFlatGaussian.m
+++
@@ -1,7 +0,0 @@
-function gaussian = createFlatGaussian(X,Y,x, y, sigma, normalize)
-gaussian = exp(- ((X-x(1)).^2 + (Y-y(1)).^2)./(2*sigma(1)).^2);
-    if normalize
-        gaussian = gaussian / sum(gaussian(:));
-    end
-gaussian = reshape(gaussian, length(X)*length(X), []);
-end
--- matlab_scripts/functions/createRFs.m
+++
@@ -1,5 +0,0 @@
-function [RF, RFvec] = createRFs(X,Y,x, y, sigma)
-RF = exp(- ((X-x(1)).^2 + (Y-y(1)).^2)./(2*sigma(1)).^2);
-RF = RF / sum (RF(:)); % normalize to sum of 1
-RFvec = reshape(RF,  1, length(X)*length(Y));
-end
